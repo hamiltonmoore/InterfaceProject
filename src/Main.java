1048f87208d032e95d6e89e41ad1099204da3f23
hamilton moore <hamlmoor@gmail.com>
2017-09-21 17:49:47 -0500

    removed some bugs/typos

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+
     public static void main(String[] args) {
 
 
@@ -45,7 +46,7 @@ public class Main {
                 System.out.println("punch");
             }
         }
-        Pilot pilot = new Pilot;
+        Pilot pilot = new Pilot();
         pilot.shoot();
     }
 }
